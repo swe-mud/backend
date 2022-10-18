f7d4108f4e080811e5beb92dde2fbd8cad3a71c3
Niklas Bieda <niklas.bieda@gmail.com>
2022-10-19 01:29:08 +0200

    Inventory is now initialized with 'empty' item slots. Now it's possible to specify at which slot in your inventory you want to put an item

--- src/classes/Inventory.ts
+++ src/classes/Inventory.ts
@@ -7,18 +7,26 @@ export default class Inventory {
     constructor() {
         this.maxInventorySlots = 5;
         this.items = [];
+        for (let i: number = 0; i<this.maxInventorySlots; i++) {
+            this.items[i] = new Item("empty", 0, 0, 0)
+        }
     }
 
     listInventory(): Item[] {
         return this.items;
     }
 
-    addItem(item: Item): void {
-        if (this.items.length < this.maxInventorySlots) {
-            this.items.push(item)
+    addItem(item: Item, slot: number): void {
+        if (slot < this.maxInventorySlots && slot > 0) {
+            if (this.items[slot].getDescription == "empty") {
+                this.items[slot-1] = item;
+            }
+            else {
+                throw new Error("There is already an item in that slot");
+            }
         }
         else {
-            throw new Error("Inventory is full!")
+            throw new Error("Your bag has not that many slots, you cannot add an item here!")
         }
     } 
 
@@ -28,10 +36,15 @@ export default class Inventory {
 
     destroyItem(slot: number): void {
         if (this.items.length <= slot && slot > 0) {
-            this.items.splice(slot-1, 1)
+            if (this.items[slot].getDescription != "empty") {
+                this.items[slot] = new Item("empty", 0, 0, 0)
+            }
+            else {
+                throw new Error("You have no item in that slot!")
+            }
         }
         else {
-            throw new Error("Your bag has not that many spaces, there is no item you can destroy")
+            throw new Error("Your bag has not that many slots, there is no item you can destroy!")
         } 
     }
 }
