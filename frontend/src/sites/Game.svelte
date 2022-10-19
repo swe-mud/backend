d1db8b138c35fb31e6013814485fa0aee9046e4d
martincodes <62392843+martincodes-de@users.noreply.github.com>
2022-10-19 16:14:32 +0200

    add demo content for gamefield

--- frontend/src/sites/Game.svelte
+++ frontend/src/sites/Game.svelte
@@ -2,9 +2,24 @@
   import Div from "../components/Container.svelte";
   import Logo from "../components/Logo.svelte";
   import ItemsShortOverview from "../components/game/ItemsShortOverview.svelte";
+  import UserMessage from "../components/game/terminal/UserMessage.svelte";
+  import CommentMessage from "../components/game/terminal/CommentMessage.svelte";
 
 
   let items = [];
+  let message = "";
+  let msg = [...Array(300).keys()];
+
+  function sendMessage() {
+
+    message = "";
+    scrollToLastMessageInTerminal();
+  }
+
+  function scrollToLastMessageInTerminal() {
+    const terminal = document.getElementById("terminalmsgs");
+    terminal.scrollTop = terminal.scrollHeight;
+  }
 </script>
 
 <div class="container-fluid mt-3">
@@ -13,11 +28,41 @@
       <ItemsShortOverview items="{ items }" />
     </div>
     <div class="col-9">
-      <div class="p-3 border bg-light">Row column</div>
+      <div class="card">
+        <div class="card-header bg-primary">
+          Terminal
+        </div>
+
+        <div class="card-body" style="max-height: 500px; overflow: auto" id="terminalmsgs">
+          <ul class="list-group">
+            {#each msg as m}
+              <UserMessage />
+
+              {#if m % 2}
+                <CommentMessage text="Das ist ein Kommentar. Was willst du tun?" />
+              {/if}
+            {/each}
+          </ul>
+        </div>
+
+        <div class="card-footer bg-secondary">
+          <details class="mb-2">
+            <summary>Befehle/Info</summary>
+            Folgt
+          </details>
+
+          <div class="input-group">
+            <input type="text" bind:value={message} class="form-control" placeholder="Deine Eingabe" aria-label="Deine Eingabe" aria-describedby="button-submit">
+            <button class="btn btn-info" type="button" id="button-submit" on:click={sendMessage}>Senden</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
 
 <style>
-
+  .form-control {
+    color: white;
+  }
 </style>
