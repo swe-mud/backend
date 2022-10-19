ab1e4dae343f92466ba02d6038662aa52ab0a7f9
martincodes <62392843+martincodes-de@users.noreply.github.com>
2022-10-19 15:44:30 +0200

    add description and entrytext

--- frontend/src/sites/Login.svelte
+++ frontend/src/sites/Login.svelte
@@ -19,10 +19,18 @@
       Start game
     </button>
 
-    <div class="p-2 mt-3 rounded-3 mb-3" style="max-width: 50%; background-color: darkgrey; color: black">
-      <b>Erklärung des Spiels:</b> Dieses Spiel ist rein textuell. Du bekommst als Spieler Text ausgegeben, in welchem die Story
-      erzählt wird und du Handlungsmöglichkeiten vorgeschlagen bekommst. Bitte lese Aufmerksam und folge den Anweisungen
-      für ein maximales Spielerlebnis.
+    <div class="p-2 mt-3 rounded-3 mb-3 text-start" style="max-width: 50%; background-color: darkgrey; color: black">
+      Hast du Bock ein duales Studium der Informatik an der HWR Berlin zu absolvieren? Nein? Wir auch nicht.
+      <br>
+      Wenn du aber stattdessen Lust hast, dich durch Dungeons zu kämpfen, Professor:innen zu besiegen und deine Skills
+      zu verbessern, dann gib jetzt deine Matrikelnummer und deinen Namen ein und leg los!
+      <br><br>
+      <details>
+        <summary>Erklärung des Spiels</summary>
+        Dieses Spiel ist rein textuell. Du bekommst als Spieler Text ausgegeben, in welchem die Story
+        erzählt wird und du Handlungsmöglichkeiten vorgeschlagen bekommst. Bitte lese Aufmerksam und folge den Anweisungen
+        für ein maximales Spielerlebnis.
+      </details>
     </div>
 
     <div class="d-flex justify-content-between" style="max-width: 50%">
